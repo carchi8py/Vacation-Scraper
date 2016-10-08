eb243303f5919b482d18eb57af2f9d80a6c93b90
Chris Archibald <carchi8py@gmail.com>
2016-10-08 12:30:46 -0700

    Add database for flights

--- vacation/database_setup.py
+++ vacation/database_setup.py
@@ -15,6 +15,14 @@ class Cruise(Base):
     depart = Column(String(250))
     nights = Column(Integer)
     price = Column(Integer)
+    
+class Flight(Base):
+    __tablename__ = 'cruise'
+    id = Column(Integer, primary_key=True)
+    price = Column(Integer)
+    start = Column(String(250))
+    cruise_id = Column(Integer, ForeignKey('cruise.id'))
+    cruise = relationship(Cruise)
 
 engine = create_engine('sqlite:///curise.db')
 Base.metadata.create_all(engine)
