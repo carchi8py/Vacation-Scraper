92dae2c3830f9342eabfb676799177feb69a8219
Chris Archibald <carchi8py@gmail.com>
2016-10-07 19:56:01 -0700

    Initial structure to generate website

---
+++ vacation/data/cruise.py
@@ -0,0 +1,11 @@
+
+#Destination 2 = Alaska
+destination = ['2']
+
+#The dates for the curise
+departuredate = ['2007-7', '2007-8']
+
+#54 = Seattle 
+#49 = San francisco
+#60 = Vancouver 
+dport1 = ['54', '49', '60']
