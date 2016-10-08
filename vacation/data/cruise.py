4af311a5bfe410456bf7f5088a71d6a4a3aaea69
Chris Archibald <carchi8py@gmail.com>
2016-10-07 20:21:39 -0700

    Get each tr that contain a cruise

--- vacation/data/cruise.py
+++ vacation/data/cruise.py
@@ -3,9 +3,15 @@
 destination = ['2']
 
 #The dates for the curise
-departuredate = ['2007-7', '2007-8']
+departuredate = ['2017-7', '2017-8']
 
 #54 = Seattle 
 #49 = San francisco
 #60 = Vancouver 
 dport1 = ['54', '49', '60']
+
+#URL Structure
+url_1 = "https://cruises.affordabletours.com/search/advanced_search/?destination="
+url_2 = "&departuredate="
+url_3 = '&departureday=&cruiseline1=&numnights=0&dport1='
+url_4 = '&resident='
--- vacation/vacation.py
+++ vacation/vacation.py
@@ -2,4 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 
 #import our data file
-import data.cruise
+import data.cruise as c
+
+import sys
+
+
+def main():
+    urls = create_urls()
+    for url in urls:
+        cruises = get_cruises(url)
+        
+
+def create_urls():
+    """
+    Create curise URLS
+    """
+    urls = []
+    for destination in c.destination:
+        for departuredate in c.departuredate:
+            for dport1 in c.dport1:
+                urls.append(c.url_1 + 
+                            destination + 
+                            c.url_2 + 
+                            departuredate + 
+                            c.url_3 +
+                            dport1 + 
+                            c.url_4)
+    return urls
+
+def get_cruises(url):
+    r = requests.get(url)
+    c = r.content
+    soup = BeautifulSoup(c, "html.parser")
+    all  = soup.find("table", {"class": "cruise-search-table"})
+    cruises = all.find("tbody").find_all("tr")
+    for cruise in cruises:
+        get_cruise_data(cruise)
+
+def get_cruise_data(cruise):
+    return cruise
+
+if __name__ == '__main__':
+    main()
